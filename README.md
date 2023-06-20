a99c9aad83a8c6e4352878d6c8fa72a90f8b2291
thestrugglingblack <zurihunter92@gmail.com>
2023-06-20 00:21:41 -0400

    add: Added README and .env.template. Created script and dependency file for downloading the data

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# wisd-data-2023
-This codebase holds a script for everyone to download the data for Women in Sports Data 2023 hackathon.
+```text
+
+  ___ ___ ___ _______ ______       ______         __             _______ _______ _______ _______ 
+ |   Y   |   |   _   |   _  \     |   _  \ .---.-|  |_.---.-.   |       |   _   |       |   _   |
+ |.  |   |.  |   1___|.  |   \    |.  |   \|  _  |   _|  _  |   |___|   |.  |   |___|   |___|   |
+ |. / \  |.  |____   |.  |    \   |.  |    |___._|____|___._|    /  ___/|.  |   |/  ___/ _(__   |
+ |:      |:  |:  1   |:  1    /   |:  1    /                    |:  1  \|:  1   |:  1  \|:  1   |
+ |::.|:. |::.|::.. . |::.. . /    |::.. . /                     |::.. . |::.. . |::.. . |::.. . |
+ `--- ---`---`-------`------'     `------'                      `-------`-------`-------`-------'
+                                                                                                 
+```
+
+
+## Table of Contents
+* [Overview](#)
+* [Prerequisites](#)
+* [Download Data](#download-data)
+
+## Overview
+This is our getting started codebase where you will be provided a script and steps to download the file. 
+
+## Prerequisites
+* Python v3.10
+* AWS Credentials
+
+## Download Data
+Verify that you have installed Python version 3.8 or later and have a copy of the SportsRadar AWS Credentials.
+
+### Setup Virtual Environment
+1. Run `python -m venv venv` to establish virtual environment.
+2. Run `source venv/bin/activate` to activate python virtual environment.
+3. Run `pip install -r requirements.txt` to download script dependencies
+
+### Running the script
+1. Create a `.env` file at the root of the codebase
+2. Copy the contents of `.env.template` into the newly created `.env` file.
+3. Update the following values based on the instructions provided in the **#general** channel of the slack.
+```text
+AWS_BUCKET_NAME
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+```
+> 📝: FILE_PATH_FOR_DATA set it to whatever file path you wish to store the data.
+
+4. Run `python download-data.py` and the script will proceed to download all the data from the SportsRadar S3 bucket. 
---
+++ download-data.py
@@ -0,0 +1,63 @@
+import boto3
+from dotenv import load_dotenv
+import os
+from tqdm import tqdm
+
+load_dotenv()
+
+try:
+
+    '''
+    1. CHECK TO SEE IF ENVIRONMENT VARIABLES ARE SET.
+    '''
+    bucket_name = os.getenv('AWS_BUCKET_NAME')
+    if not bucket_name:
+        raise Exception("AWS_BUCKET_NAME environment variable is not set.")
+    game_ids = os.getenv('GAME_IDS')
+    if not game_ids:
+        raise Exception("GAME_IDS environment variable is not set.")
+    game_ids = game_ids.split(',')
+    download_directory = os.getenv('FILE_PATH_FOR_DATA')
+    if not download_directory:
+        raise Exception("FILE_PATH_FOR_DATA environment variable is not set.")
+
+
+    s3_client = boto3.client('s3')
+
+    '''
+    02. DOWNLOAD FILES FOR EACH GAME.
+    '''
+    for game_id in game_ids:
+
+        # Download metadata
+        metadata_prefix = f'metadata/{game_id}'
+        metadata_response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=metadata_prefix)
+
+        if 'Contents' in metadata_response:
+            for obj in metadata_response['Contents']:
+                object_key = obj['Key']
+                local_file_path = os.path.join(download_directory, object_key)
+
+                # Check if directory exist before download or it will fail.
+                os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+                with tqdm(total=obj['Size'], unit='B', unit_scale=True, unit_divisor=1024, desc=object_key) as progress_bar:
+                    s3_client.download_file(bucket_name, object_key, local_file_path,
+                                            Callback=lambda bytes_transferred: progress_bar.update(bytes_transferred))
+
+        # Download tracking/events
+        game_prefix = f'games/{game_id}'
+        game_response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=game_prefix)
+
+        if 'Contents' in game_response:
+            for obj in game_response['Contents']:
+                object_key = obj['Key']
+                local_file_path = os.path.join(download_directory, object_key)
+
+                # Check if directory exist before download or it will fail.
+                os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
+                with tqdm(total=obj['Size'], unit='B', unit_scale=True, unit_divisor=1024, desc=object_key) as progress_bar:
+                    s3_client.download_file(bucket_name, object_key, local_file_path,
+                                            Callback=lambda bytes_transferred: progress_bar.update(bytes_transferred))
+
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
